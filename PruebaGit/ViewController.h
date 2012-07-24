b4d5761489ae2048e1e65f47e0a5234fc2814478
Javi <Javi@iMac-de-Imagina.local>
2012-07-24 13:42:37 +0200

    Initial Commit

---
+++ PruebaGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PruebaGit
+//
+//  Created by Javier Hernandez on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
